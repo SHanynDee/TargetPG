bee0b24398a7946bc285676469e9def2ca4a0e8e
SHanynDee <57260121+SHanynDee@users.noreply.github.com>
2020-09-16 19:04:09 +0200

    Add files via upload

---
+++ Cr50/2.5MeV_thin/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/radioprotection.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/radioprotection.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/ActionInitialization.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/ActionInitialization.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/AnalysisManager.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/AnalysisManager.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/DetectorConstruction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/DetectorConstruction.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/DetectorConstructionMessenger.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/DetectorConstructionMessenger.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/EventAction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/EventAction.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/PhysicsList.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/PhysicsList.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/PhysicsListMessenger.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/PhysicsListMessenger.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/PrimaryGeneratorAction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/RunAction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/RunAction.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/StackingAction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/StackingAction.cc.o"
+  "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/src/SteppingAction.cc" "/home/shan/CHPC Results/Cr50/2.5MeV_thin_MYPC/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/shan/Programs/Geant4.10.05/geant4.10.05.p01-installMT/include/Geant4"
+  "/usr/include/qt4"
+  "/usr/include/qt4/QtCore"
+  "/usr/include/qt4/QtGui"
+  "/usr/include/qt4/QtOpenGL"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cr50/2.5MeV_thin/build_0.00003mm_thin/CMakeFiles/radioprotection.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radioprotection.dir/radioprotection.cc.o"
+  "CMakeFiles/radioprotection.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/radioprotection.dir/src/AnalysisManager.cc.o"
+  "CMakeFiles/radioprotection.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/radioprotection.dir/src/DetectorConstructionMessenger.cc.o"
+  "CMakeFiles/radioprotection.dir/src/EventAction.cc.o"
+  "CMakeFiles/radioprotection.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/radioprotection.dir/src/PhysicsListMessenger.cc.o"
+  "CMakeFiles/radioprotection.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/radioprotection.dir/src/RunAction.cc.o"
+  "CMakeFiles/radioprotection.dir/src/StackingAction.cc.o"
+  "CMakeFiles/radioprotection.dir/src/SteppingAction.cc.o"
+  "radioprotection.pdb"
+  "radioprotection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/radioprotection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
