b5c3dc5e168a0265cfd5ca40d49e7a9d1d79b3a3
SHanynDee <57260121+SHanynDee@users.noreply.github.com>
2020-09-16 19:23:18 +0200

    Add files via upload

---
+++ Cr50/UpdatedLoadPlotNtuple.C
@@ -0,0 +1,55 @@
+{
+
+TFile f("radioprotection.root");
+TDirectory *dir= (TDirectory *) f.Get("GammaVertex"); 
+
+//TLeaf *Leaf1 = (TTree *) GammaVertex->GetLeaf("Ekin")
+//TTree *Leaf2 = (TTree *) GammaVertex->GetLeaf("FinalA")
+//TTree *Leaf3 = (TTree *) GammaVertex->GetLeaf("FinalZ")
+//TTree *Leaf4 = (TTree *) GammaVertex->GetLeaf("EkinProton")
+
+
+
+//TNtuple *ntuple1 = (TNtuple *) dir->Get("Ekin");
+//TNtuple *ntuple2 = (TNtuple *) dir->Get("FinalA");
+//TNtuple *ntuple3 = (TNtuple *) dir->Get("FinalZ");
+//TNtuple *ntuple4 = (TNtuple *) dir->Get("EkinProton"); 
+
+int numberOfBinsX = 1000;
+int numberOfBinsY = 1000;
+int numberOfBinsZ = 1000;
+
+int Xmin = 0;
+int Xmax = 10;
+int Ymin = 0;
+int Ymax = 1000;
+int Zmin = 0;
+int Zmax = 1000;
+
+//the type of histogram, how many variable, min and max values plus size of bins
+TH1F *edep1Distribution = new TH1F("h0", "Primary Particle Energy Spectrum; Energy (Mev);Frequency", numberOfBinsX, Xmin, Xmax);       //Edep // binning, xmin, xmax, along x direction
+
+//the type of histogram, how many variable, min and max values plus size of bins
+TH1F *edep11Distribution = new TH1F("h1", "Energy deposition; Edep (kev);Frequency", numberOfBinsX, Xmin, Xmax);       //Edep // binning, xmin, xmax, along x direction
+
+
+TH2F* edep2DDistribution = new TH2F("h2", "Energy deposited by Ions; Z; Edep (keV)", numberOfBinsX, Xmin, Xmax,numberOfBinsY, Ymin, Ymax);   
+	 // Z     // binning, xmin, xmax, along x direction
+ 	//Edep   // binning, xmin, xmax, along y direction
+
+
+TH3F *edep3DDistribution = new TH3F("h3", "3Dedep; Edep (keV) ; A; Z", numberOfBinsX, Xmin, Xmin, numberOfBinsY, Ymin, Ymax,  numberOfBinsZ, Zmin, Zmax); 
+// edep  // binning, xmin, xmax, along x direction
+// A     //binning, xmin, xmax, along y direction
+// Z     //binning, xmin, xmax, along z direction
+
+
+//Plot Primary Energy of Incident Particle
+//GammaVertex->Draw("Ekin>>h0","","");
+//Plot Energy Deposition within SV
+GammaVertex->Draw("EkinProton>>h1", "", "");
+//Plot 2D/3D Histogram of energy with particle type using A and Z
+GammaVertex->Draw("FinalZ:EkinProton>>h2", "", "");
+//GammaVertex->Draw("FinalZ:FinalA:EkinProton>>h3", "", "");
+
+}			
